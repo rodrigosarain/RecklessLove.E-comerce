707a4828d780bc22ec57bfc15cd080e3827684e3
MAKEOUTHILL <rodriisaraiin@gmail.com>
2023-09-21 19:43:16 -0300

    subida de archivos

---
+++ JS/contact.js
@@ -0,0 +1,66 @@
+// MENU HAMBURGUESA
+const hamburgerMenu = document.querySelector('.header__menu');
+const modalNavbar = document.querySelector('.modal-navbar__background');
+const closeModalNavbar = document.querySelector('.modal-navbar__close');
+
+modalNavbar.style.display = 'none'
+
+hamburgerMenu.addEventListener('click', ()=>{
+    console.log('abrir modal');
+    modalNavbar.style.display = 'block';
+});
+
+closeModalNavbar.addEventListener('click', ()=>{
+    modalNavbar.style.display = 'none';
+});
+
+$(document).ready(function(){
+
+    $('#btnSend').click(function(){
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if( $('#names').val() == '' ){
+            errores += '<p>✖ Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if( $('#email').val() == '' ){
+            errores += '<p>✖ Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>✖ Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Close</span>'+
+                                    '</div>'+
+                                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        });
+    });
+
+});
---
+++ JS/main.js
@@ -0,0 +1,194 @@
+// suma de items 
+
+let minusBtn = document.querySelector('.input__minus');
+let plusBtn = document.querySelector ('.input__plus');
+let userInput = document.querySelector('.input__number');
+
+
+let userInputNumber = 0;
+
+plusBtn.addEventListener('click', ()=>{
+    userInputNumber++;
+    userInput.value = userInputNumber;
+    console.log(userInputNumber);
+});
+
+
+minusBtn.addEventListener('click', ()=>{
+    userInputNumber--;
+    if (userInputNumber <=0) {
+        userInputNumber =0;
+    }
+    userInput.value = userInputNumber;
+    console.log(userInputNumber);
+});
+// fin
+
+// agregar total al carrito 
+
+const addToCartBtn = document.querySelector('.details__button');
+let cartNotification = document.querySelector('.header__cart--notification');
+let lastValue = parseInt(cartNotification.innerText);
+
+addToCartBtn.addEventListener('click', ()=>{
+    lastValue = lastValue + userInputNumber;
+
+    cartNotification.innerText = lastValue;
+    cartNotification.style.display = 'block';
+    drawProductInmodal();
+    
+});
+
+// mostrar el modal con el detalle del carrito
+const cartIconBtn = document.querySelector ('.header__cart');
+const cartModal = document.querySelector ('.cart-modal');
+const productContainer = document.querySelector ('.cart-modal__checkout-container');
+
+cartIconBtn.addEventListener('click', ()=>{
+    cartModal.classList.toggle ('show');
+
+    if (lastValue === 0){
+    productContainer.innerHTML = `<p class = "cart-empty">Your cart is empty</p>`;
+    }else{
+        drawProductInmodal();
+    }
+});
+
+// borrar el contenido del carrito
+
+function deleteProduct() {
+    const deleteItemsBtn = document.querySelector ('.cart-modal__delete');
+    deleteItemsBtn.addEventListener('click', ()=>{
+        productContainer.innerHTML = `<p class = "cart-empty">Your cart is empty</p>`;
+        lastValue = 0;
+        cartNotification.innerText = lastValue;
+    });
+}
+
+// cambiar imagenes al precionar boton flecha
+const imageContainer = document.querySelector('.gallery__image-container');
+const prevGalleryBtn = document.querySelector('.gallery__previus');
+const nextGalleryBtn = document.querySelector('.gallery__next');
+
+let imgIndex = 1;
+
+nextGalleryBtn.addEventListener('click', ()=>{
+    changeNextImage(imageContainer );
+});
+
+prevGalleryBtn.addEventListener('click', ()=>{
+    changePreviusImage(imageContainer );
+});
+
+// mostrar el modal desktop
+
+const modalImages = document.querySelector('.modal-gallery__background');
+const closeModalBtn = document.querySelector('.modal-gallery__close');
+
+imageContainer.addEventListener('click', ()=>{
+    modalImages.style.display = 'grid';
+});
+
+closeModalBtn.addEventListener('click', ()=>{
+    modalImages.style.display = 'none';
+});
+
+// cambiar imagenes desde los thumnails
+let thumnails = document.querySelectorAll ('.gallery__thumnails');
+thumnails = [...thumnails]
+
+thumnails.forEach(thumnail => {
+    thumnail.addEventListener('click', event=>{
+        console.log(event.target.id);
+        imageContainer.style.backgroundImage = `url('../MEDIA/image-product-${event.target.id}.png')`
+
+    })
+});
+
+// cambiar img desde los thumnails de el modal
+let modalThumnails = document.querySelectorAll ('.modal-gallery__thumnails');
+const modalImageContainer = document.querySelector ('.modal-gallery__image-container');
+modalThumnails = [...modalThumnails]
+
+modalThumnails.forEach(modalThumnail => {
+    modalThumnail.addEventListener('click', event=>{
+        console.log(event.target.id.slice(-1));
+        modalImageContainer.style.backgroundImage = `url('../MEDIA/image-product-${event.target.id.slice(-1)}.png')`
+    })
+});
+
+// cambiar imagenes del modal desde botones
+const nextModalBtn = document.querySelector ('.modal-gallery__next');
+const prevModalBtn = document.querySelector ('.modal-gallery__previus');
+
+nextModalBtn.addEventListener('click', ()=>{
+    changeNextImage(modalImageContainer);
+});
+
+prevModalBtn.addEventListener('click', ()=>{
+    changePreviusImage(modalImageContainer);
+});
+
+// mostrar navbar menu hamburguesa 
+const hamburgerMenu = document.querySelector('.header__menu');
+const modalNavbar = document.querySelector('.modal-navbar__background');
+const closeModalNavbar = document.querySelector('.modal-navbar__close');
+
+modalNavbar.style.display = 'none'
+
+hamburgerMenu.addEventListener('click', ()=>{
+    console.log('abrir modal');
+    modalNavbar.style.display = 'block';
+});
+
+closeModalNavbar.addEventListener('click', ()=>{
+    modalNavbar.style.display = 'none';
+});
+
+
+// funciones
+function drawProductInmodal() {
+    productContainer.innerHTML = ` 
+    <div class="cart-modal__details-container">
+    <img class="cart-modal__image" src="../MEDIA/image-product-1.png" alt="">
+    <div>
+        <p class="cart-modal__product">Morality Shirt</p>
+        <p class="cart-modal__price">$125 x3 <span>$375.00</span></p>
+    </div>
+    <img  class="cart-modal__delete" src="../MEDIA/borrar.png" alt="">
+</div>
+<div class="cart-modal__pay-off">
+    <a href="../CardPay.html"> <img class="cart-modal__pay"  src="../MEDIA/punto-de-venta.png" alt=""></a>
+</div>
+</div>`
+    deleteProduct();
+    let pricesModal = document.querySelector ('.cart-modal__price');
+    pricesModal.innerHTML = `$125 x${lastValue} <span>$${lastValue*125}.00</span>`;
+}
+
+function changeNextImage(imgContainer) {
+    if (imgIndex == 4){
+        imgIndex = 1
+    }else{
+        imgIndex++;
+    }
+    imgContainer.style.backgroundImage = `url('../MEDIA/image-product-${imgIndex}.png')`
+   
+}
+
+
+function changePreviusImage(imgContainer) {
+    if (imgIndex == 1){
+        imgIndex = 4
+    }else{
+        imgIndex--;
+    }
+    imgContainer.style.backgroundImage = `url('../MEDIA/image-product-${imgIndex}.png')`
+}
+
+
+// btn chekout
+const checkOutBtn = document.querySelector('.cart-modal__chekout');
+checkOutBtn.addEventListener('click', ()=>{
+    window.location.href = '../CardPay.html';
+})
